9c438fca6f7e811a076a98bb8f3c2ae67f30e737
ALEXIS SAUL LOPEZ REYNAGA <a344255@alumnos.uaslp.mx>
2025-02-24 21:42:43 -0600

    Ejercicio1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './Component/header/header.component';
+import { CounterComponent } from './Component/counter/counter.component';
+import { NameListComponent } from './Component/name-list/name-list.component';
+import { FooterComponent } from './Component/footer/footer.component';
+import { ContentComponent } from './Component/content/content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CounterComponent, NameListComponent,FooterComponent,ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
